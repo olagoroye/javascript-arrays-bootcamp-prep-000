484b4a760bca642456936c0bd1495f9afc6d21eb
olusola olagoroye <solaolagoroye@gmail.com>
2019-10-28 21:18:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,4 +33,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length -1);
+  return array;
+}
