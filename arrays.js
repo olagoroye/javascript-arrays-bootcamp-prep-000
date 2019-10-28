386d135cef72adb89f89193f258a0870b8e1b8a9
olusola olagoroye <solaolagoroye@gmail.com>
2019-10-28 21:03:24 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, elemen){
+  array.unshift(element);
+  return array;
+}
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
+function accessElementInArray(array, index){
+  return array[index];
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice[1];
+  return array;
+}
+function 
