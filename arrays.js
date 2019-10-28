e9f2d20840622506ec77e2521f5435caf1d7da64
olusola olagoroye <solaolagoroye@gmail.com>
2019-10-28 21:25:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice[-1];
+  array.slice[1];
   return array;
 }
 function destructivelyRemoveElementFromEndOfArray(array){
